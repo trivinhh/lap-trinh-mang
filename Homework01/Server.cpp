ce02ed1acbc59ca805fb4d711915f49b234388f8
trivinh <trivinhng@gmail.com>
2022-04-24 23:25:26 +0800

    baitap1

---
+++ Homework01/Server.cpp
@@ -0,0 +1,55 @@
+﻿// Server1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32")
+
+int main(int argc, char* argv[])
+{
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    SOCKET listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    SOCKADDR_IN addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(atoi(argv[1]));
+
+    bind(listener, (SOCKADDR*)&addr, sizeof(addr));
+    listen(listener, 5);
+
+    SOCKET client = accept(listener, NULL, NULL);
+
+    char buf[256];
+    int ret;
+
+    // Doc xau chao tu file
+    FILE* f = fopen(argv[2], "r");
+    fgets(buf, sizeof(buf), f);
+    fclose(f);
+
+    // Gui sang client
+    send(client, buf, strlen(buf), 0);
+
+    // Nhan du lieu tu client va ghi vao file
+    f = fopen(argv[3], "wb");
+    while (1)
+    {
+        ret = recv(client, buf, sizeof(buf), 0);
+        if (ret <= 0)
+            break;
+        fwrite(buf, 1, ret, f);
+    }
+    fclose(f);
+
+    closesocket(client);
+    closesocket(listener);
+    WSACleanup();
+}
---
+++ Homework01/client-gui-du-lieu-thong-tin-may-tinh.cpp
@@ -0,0 +1,55 @@
+﻿// Client2.cpp : BT1
+//
+
+#include <stdio.h>
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32")
+
+int main()
+{
+    /*WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+    SOCKET client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    SOCKADDR_IN addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(argv[1]);
+    addr.sin_port = htons(atoi(argv[2]));*/
+
+    // connect(client, (SOCKADDR*)&addr, sizeof(addr));
+
+    char buf[256];
+
+    char computerName[256];
+    DWORD nameLength = sizeof(computerName);
+    GetComputerNameA(computerName, &nameLength);
+
+    char driveLetters[256];
+    int ret = GetLogicalDriveStringsA(sizeof(driveLetters), driveLetters);
+
+    DWORD sectorsPerCluster, bytesPerSector, numberOfFreeClusters, totalNumberOfClusters;
+
+    // Tao buffer de gui du lieu
+    char numDrives = ret / 4;
+    buf[0] = numDrives;
+    int pos = 1;
+    for (int i = 0; i < numDrives; i++)
+    {
+        // Drive letter
+        buf[pos] = driveLetters[i * 4];
+        pos += 1;
+
+        // Drive size
+        GetDiskFreeSpaceA(driveLetters + i * 4, &sectorsPerCluster, &bytesPerSector,
+            &numberOfFreeClusters, &totalNumberOfClusters);
+
+        float size = (float)totalNumberOfClusters * (float)sectorsPerCluster * (float)bytesPerSector / (float)(1 << 30);
+        memcpy(buf + pos, &size, sizeof(size));
+        pos += sizeof(size);
+    }
+    memcpy(buf + pos, computerName, nameLength + 1);
+    pos += nameLength + 1;
+
+}
